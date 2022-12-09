72176e598d83dc3e1d5867421dd3f55e076b2a62
Simon <simon.strykers@gmail.com>
2022-12-09 01:58:37 +0100

    Amélioration de l'algorithme pour le problème 8 d'Euler

--- src/be/technifutur/decouverte/euler/problem8.java
+++ src/be/technifutur/decouverte/euler/problem8.java
@@ -3,37 +3,22 @@ package be.technifutur.decouverte.euler;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class problem8 {
     public static void main(String[] args) {
         TableauNombre tableauNombre = new TableauNombre();
 
-        tableauNombre.ajoutNombres("73167176531330624919225119674426574742355349194934");
-        tableauNombre.ajoutNombres("96983520312774506326239578318016984801869478851843");
-        tableauNombre.ajoutNombres("85861560789112949495459501737958331952853208805511");
-        tableauNombre.ajoutNombres("12540698747158523863050715693290963295227443043557");
-        tableauNombre.ajoutNombres("66896648950445244523161731856403098711121722383113");
-        tableauNombre.ajoutNombres("62229893423380308135336276614282806444486645238749");
-        tableauNombre.ajoutNombres("30358907296290491560440772390713810515859307960866");
-        tableauNombre.ajoutNombres("70172427121883998797908792274921901699720888093776");
-        tableauNombre.ajoutNombres("65727333001053367881220235421809751254540594752243");
-        tableauNombre.ajoutNombres("52584907711670556013604839586446706324415722155397");
-        tableauNombre.ajoutNombres("53697817977846174064955149290862569321978468622482");
-        tableauNombre.ajoutNombres("83972241375657056057490261407972968652414535100474");
-        tableauNombre.ajoutNombres("82166370484403199890008895243450658541227588666881");
-        tableauNombre.ajoutNombres("16427171479924442928230863465674813919123162824586");
-        tableauNombre.ajoutNombres("17866458359124566529476545682848912883142607690042");
-        tableauNombre.ajoutNombres("24219022671055626321111109370544217506941658960408");
-        tableauNombre.ajoutNombres("07198403850962455444362981230987879927244284909188");
-        tableauNombre.ajoutNombres("84580156166097919133875499200524063689912560717606");
-        tableauNombre.ajoutNombres("05886116467109405077541002256983155200055935729725");
-        tableauNombre.ajoutNombres("71636269561882670428252483600823257530420752963450");
+        tableauNombre.ajoutNombres("7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450");
 
-        int result = tableauNombre.findGreatestProduct(13);
+        int greatestProduct = tableauNombre.findGreatestProduct(13);
+
+        System.out.println(greatestProduct);
     }
 
     private static class TableauNombre {
-        private List<Integer> nombres = new ArrayList<>();
+        public static final int TAILLE_LISTE = 50;
+        private final List<List<Integer>> nombres = new ArrayList<>();
 
         public void ajoutNombres(String s) {
             int taille = s.length();
@@ -41,24 +26,56 @@ public class problem8 {
 
             for (int i = 0; i < taille; i++) {
                 nouveauxNombres.add(Character.getNumericValue(s.charAt(i)));
+
+                // Ce code permet de diviser le nombre en chaine égales
+                if ((i+1) % TAILLE_LISTE == 0) {
+                    nombres.add(nouveauxNombres);
+                    nouveauxNombres = new ArrayList<>();
+                }
             }
 
-            nombres.addAll(nouveauxNombres);
+            if (!nouveauxNombres.isEmpty())
+                nombres.add(nouveauxNombres);
         }
 
         public int findGreatestProduct(int quantiteNombresAdjacents) {
             int result = 0;
             int calc = 0;
-            int lengthNumbers = nombres.size();
 
-            for (int i = 0; i < lengthNumbers; i++) {
-                calc = ProductFromArray(nombres.subList(i, i + quantiteNombresAdjacents).toArray());
+            for (List l :
+                    nombres) {
+                calc = findGreatestProductInList(l,quantiteNombresAdjacents);
+
+                if (calc > result)
+                    result = calc;
             }
-            
+
             return result;
         }
 
-        private int ProductFromArray(Object[] toArray) {
+        public static int findGreatestProductInList(List<Integer> list, int quantiteNombresAdjacents) {
+            int result = 0;
+            int calc = 0;
+            int lengthNumbers = list.size();
+
+            for (int i = 0; i <= lengthNumbers - quantiteNombresAdjacents; i++) {
+                calc = ProductFromArray(list.subList(i, i + quantiteNombresAdjacents).toArray(new Integer[0]));
+
+                if (calc > result)
+                    result = calc;
+            }
+
+            return result;
+        }
+
+        private static <T> int ProductFromArray(T[] toArray) {
+            List<Integer> list = Arrays.stream(toArray)
+                    .map(Object::toString) // element -> element.toString()
+                    .map(Integer::parseInt) // str -> Integer.parseInt(str)
+                    .toList();
+
+            return list.stream()
+                    .reduce(1, (a, b) -> a * b);
         }
 
         @Override
