d88a7dade7ca54a37da74d47bb31accb475a1afc
Simon <simon.strykers@gmail.com>
2022-12-15 02:54:12 +0100

    ajout d'exercices

--- src/be/technifutur/decouverte/euler/problem26.java
+++ src/be/technifutur/decouverte/euler/problem26.java
@@ -19,27 +19,33 @@ public class problem26 {
         List<Integer> listPrimeNumber = listPrimeNumber(DECIMALS_SCALE);
         listPrimeNumber.removeIf(p -> p >= DECIMALS_SCALE);
 
+        // Ne calcule qu'à partir des nombres premiers
         for (Integer i: listPrimeNumber) {
             BigDecimal dividend = new BigDecimal(1);
             BigDecimal divisor = new BigDecimal(i);
             BigDecimal result = dividend.divide(divisor, DECIMALS_SCALE, RoundingMode.HALF_UP).stripTrailingZeros();
 
+            // Ne calcule que pour ceux qui ont une suite de décimales qui a l'air infinie
             if (result.toString().length() == DECIMALS_SCALE + 2) {
                 boolean cycleFound = false;
                 int cycleLength = 0;
 
+                // Remet le nombre à l'unité, afin qu'il puisse passer dans la vérification par modulo 1
                 while (result.compareTo(BigDecimal.valueOf(1)) < 0)
                     result = result.scaleByPowerOfTen(1);
 
                 BigDecimal number1 = bigDecimalTruncate(new BigDecimal(String.valueOf(result)), 1);
                 BigDecimal number2 = bigDecimalTruncate(result, 1);
+
+                // Recherche la taille de la suite probable
                 while (cycleLength < DECIMALS_SCALE - 10 && !cycleFound) {
                     cycleLength++;
                     number1 = number1.scaleByPowerOfTen(1);
                     number2 = bigDecimalTruncate(number2, 1);
 
-                    if (number1.subtract(number2).remainder(BigDecimal.valueOf(2)).compareTo(BigDecimal.valueOf(0)) == 0) {
-                        System.out.printf("%d: cycle de %d\n", i, cycleLength);
+                    // Si par la soustraction d'un nombre plus élevé de E^cycleLength il n'y a plus de reste avec le modulo 1, c'est alors la taille de la suite
+                    if (number1.subtract(number2).remainder(BigDecimal.valueOf(1)).compareTo(BigDecimal.valueOf(0)) == 0) {
+                        System.out.println(i);
                         cycles.put(i, cycleLength);
                         cycleFound = true;
                     }
@@ -50,12 +56,12 @@ public class problem26 {
 
     }
 
-    public static List<Integer> listPrimeNumber(int length) {
+    public static List<Integer> listPrimeNumber(int qttPrimeNumber) {
         List<Integer> listeNombrePremier = new ArrayList<>();
 
         int compteur = 3;
         int sizeList = 1;
-        while (sizeList <= length) {
+        while (sizeList <= qttPrimeNumber) {
             boolean isPremier = true;
             Iterator<Integer> iterator = listeNombrePremier.iterator();
 
---
+++ src/be/technifutur/decouverte/euler/problem30.java
@@ -0,0 +1,9 @@
+package be.technifutur.decouverte.euler;
+
+public class problem30 {
+    public static void main(String[] args) {
+        for (int num = 1; num < 10000; num++) {
+
+        }
+    }
+}
