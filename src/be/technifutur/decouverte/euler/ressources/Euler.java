3d2866150e69d9f06219b5689c7bd8f9ceab9cbd
Simon <simon.strykers@gmail.com>
2022-12-21 01:47:02 +0100

    ajout d'exercices

---
+++ src/be/technifutur/decouverte/euler/ressources/Euler.java
@@ -0,0 +1,11 @@
+package be.technifutur.decouverte.euler.ressources;
+
+public class Euler {
+    public static int[] intToDigits(int number) {
+        return String.valueOf(number).chars().map(x -> x - 48).toArray();
+    }
+
+    public static int[] intToDigits(String number) {
+        return number.chars().map(x -> x - 48).toArray();
+    }
+}
